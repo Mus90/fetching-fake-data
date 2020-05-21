4c8b34d9a0b0c2def1e41bbf19089ab1ee8a1fc9
mustafa ali <mustafaalamin.07@gmail.com>
2020-05-21 23:26:33 +0200

    deleted some text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <h1>Hello to my whether App</h1>
       <p>
-        is simply dummy text of the printing and typesetting industry. Lorem
         Ipsum has been the industry's standard dummy text ever since the 1500s,
         when an unknown printer took a galley of type and scrambled it to make a
         type specimen book. It has survived not only five centuries, but also
