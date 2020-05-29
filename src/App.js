df7e108dba43a601ba56530f242938d44df34b08
mustafa ali <mustafaalamin.07@gmail.com>
2020-05-29 03:43:37 +0200

    fetching data from an API and present a single user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, {Component} from "react";
-import './App.scss';
+import React, { Component } from "react";
+import "./App.scss";
 import Routes from "./Routes";
 
 export default class App extends Component {
-//todo change it to functional component
+  //todo change it to functional component
   render() {
     return (
       <div className="App">
-        <Routes/>
+        <Routes />
       </div>
     );
   }
